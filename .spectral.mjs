5517017100b3575c35b0d28c6837a63f0c8742dc
sylvain@baqio.com <sylvain@baqio.com>
2024-01-24 15:06:48 +0100

    Modified 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjEyNDY/spectral.js?branch_slug=master&token=09d6e999-be5d-45de-b8d7-377c1e254f0a";
+export default { extends: ruleset };
